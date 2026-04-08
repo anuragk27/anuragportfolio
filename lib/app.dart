c4b635090bcea381fab6e4568808692d649b1f50
Anuragk27 <anuragk.c11@gmail.com>
2026-04-08 17:42:28 +0530

    Organize imports by removing unused page imports and adding section imports for About, Projects, Skills, and Contact

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+// import 'pages/home.dart';
+// import 'pages/about.dart';
+// import 'pages/projects.dart';
+// import 'pages/contact.dart';
+import '../sections/AboutPage.dart';
+import '../sections/ProjectsPage.dart';
+import '../sections/SkillsPage.dart';
+import '../sections/ContactPage.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
